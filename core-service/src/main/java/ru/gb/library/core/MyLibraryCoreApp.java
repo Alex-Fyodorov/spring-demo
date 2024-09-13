bfd3e9e1d24a347be5f9ac9306fcb96db463fa63
Alex <rassylka82@inbox.ru>
2024-09-13 20:14:51 +0300

    проверка

--- core-service/src/main/java/ru/gb/library/core/MyLibraryCoreApp.java
+++ core-service/src/main/java/ru/gb/library/core/MyLibraryCoreApp.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyLibraryCoreApp {
 
 	// http://localhost:8080/swagger-ui.html
+	// http://localhost:8080/swagger-ui.html
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyLibraryCoreApp.class, args);
